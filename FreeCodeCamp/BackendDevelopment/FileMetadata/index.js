4f5043ab11f7cd0529ba4183dbae637b695c9c5d
alexander.petrovskiy <tula147@yandex.ru>
2022-08-02 01:29:37 +0400

    #FileMetadata

---
+++ FreeCodeCamp/BackendDevelopment/FileMetadata/index.js
@@ -0,0 +1,30 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import multer from "multer";
+dotenv.config();
+
+var app = express();
+
+app.use(cors());
+app.use("/public", express.static(process.cwd() + "/public"));
+
+const upload = multer({ dest: "uploads/" });
+
+app.get("/", function (req, res) {
+    res.sendFile(process.cwd() + "/views/index.html");
+});
+
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
+    console.log(req.file);
+    res.json({
+        name: req.file.originalname,
+        type: req.file.mimetype,
+        size: req.file.size,
+    });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+    console.log("Your app is listening on port " + port);
+});
